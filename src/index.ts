d1099f24c6824c67d54898df3251753dcca7ac0b
Jonah <jnhev42@gmail.com>
2024-09-07 13:37:22 +0100

    Update .prettierrc tab width

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ console.log("hello :DDDD");
 type Side = "atk" | "def";
 
 type Strat = {
-    side: Side;
+  side: Side;
 };
 
 let a: Strat = { side: "def" };
@@ -13,24 +13,23 @@ console.log(a.side);
 import Konva from "konva";
 
 const stage = new Konva.Stage({
-    container: "container",
-    width: window.innerWidth,
-    height: window.innerHeight,
+  container: "container",
+  width: window.innerWidth,
+  height: window.innerHeight,
 });
 
 const layer = new Konva.Layer();
 stage.add(layer);
 
 const box = new Konva.Rect({
-    x: 50,
-    y: 50,
-    width: 100,
-    height: 50,
-    fill: "#00D2FF",
-    stroke: "black",
-    strokeWidth: 4,
-    draggable: true,
-    
+  x: 50,
+  y: 50,
+  width: 100,
+  height: 50,
+  fill: "#00D2FF",
+  stroke: "black",
+  strokeWidth: 4,
+  draggable: true,
 });
 layer.add(box);
 
