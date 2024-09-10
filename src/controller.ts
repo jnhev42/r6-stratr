ae17d417a5dcb6eba764abd03016bf29df4dfa04
Rowan Elsworth <rowanelsworth@gmail.com>
2024-09-10 03:30:12 +0100

    controller

---
+++ src/controller.ts
@@ -0,0 +1,20 @@
+
+import { Strat, OpConfig, Op } from "./model";
+
+export class StratController {
+  model: Strat;
+
+  constructor(model: Strat) {
+    this.model = model;
+    
+
+  }
+
+  addPiece() {
+    
+  }
+}
+
+export class PieceController {
+
+}
